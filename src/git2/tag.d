7cbe21fe3c24b7b06f3a44bef980b690ea593268
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2012-10-14 00:23:18 +0200

    add missing inlines.

--- src/git2/tag.d
+++ src/git2/tag.d
@@ -1,10 +1,23 @@
 module git2.tag;
 
+import git2._object;
 import git2.common;
 import git2.oid;
 import git2.types;
 
 extern(C):
+
+/** GIT_INLINE */
+int git_tag_lookup(git_tag** tag, git_repository* repo, const(git_oid)* id)
+{
+    return git_object_lookup(cast(git_object**)tag, repo, id, cast(git_otype)git_otype.GIT_OBJ_TAG);
+}
+
+/** GIT_INLINE */
+int git_tag_lookup_prefix(git_tag** tag, git_repository* repo, const(git_oid)* id, size_t len)
+{
+    return git_object_lookup_prefix(cast(git_object**)tag, repo, id, len, cast(git_otype)git_otype.GIT_OBJ_TAG);
+}
 
 int git_tag_create(git_oid* oid, git_repository* repo, const(char)* tag_name, const(git_object)* target, const(git_signature)* tagger, const(char)* message, int force);
 int git_tag_create_frombuffer(git_oid* oid, git_repository* repo, const(char)* buffer, int force);
@@ -14,8 +27,6 @@ void git_tag_free(git_tag* tag);
 const(git_oid)* git_tag_id(git_tag* tag);
 int git_tag_list(git_strarray* tag_names, git_repository* repo);
 int git_tag_list_match(git_strarray* tag_names, const(char)* pattern, git_repository* repo);
-int git_tag_lookup(git_tag** tag, git_repository* repo, const(git_oid)* id);
-int git_tag_lookup_prefix(git_tag** tag, git_repository* repo, const(git_oid)* id, size_t len);
 const(char)* git_tag_message(git_tag* tag);
 const(char)* git_tag_name(git_tag* tag);
 int git_tag_peel(git_object** tag_target, git_tag* tag);
