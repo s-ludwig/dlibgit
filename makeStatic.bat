eaf29c46aa8c89fa5568474d189307dce5820288
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-01-24 04:07:51 +0100

    fix static build script.

--- makeStatic.bat
+++ makeStatic.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-for /r %%i in (c\git2\*.d) do set files=%%i !files!
+for /r %%i in (git2\*.d) do set files=%%i !files!
 
 rem echo %files%
 dmd -lib -ofbin\dlibgit.lib -Isrc %files%
