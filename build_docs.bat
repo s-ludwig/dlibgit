68f66ece1fb80a3462f3316130f1d301d717bb7c
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-06-28 22:12:38 +0200

    Add ddoc files.

--- build_docs.bat
+++ build_docs.bat
@@ -4,12 +4,14 @@ setlocal EnableDelayedExpansion
 set "this_path=%~dp0"
 cd %this_path%
 
-for /r %%i in (src\git\*.d) do set files=%%i !files!
-for /r %%i in (src\git\c\*.d) do set files=%%i !files!
-for /r %%i in (src\git\c\sys\*.d) do set files=%%i !files!
+rem for /r %%i in (src\git\*.d) do echo %%i
+rem for /r %%i in (src\git\c\*.d) do echo %%i
+rem for /r %%i in (src\git\c\sys\*.d) do echo %%i
 
 rem echo %files%
-dmd -o- -D -Dddocs docs\viola.ddoc -Isrc %files%
+rem dmd -o- -D -Dddocs docs\viola.ddoc -Isrc %files%
+
+rdmd -d ..\bootDoc\generate.d src --bootdoc=docs\config --modules=docs\config\modules.ddoc --settings=docs\config\settings.ddoc --output=docs
 
 rem echo.
 echo Build ok.
