40414576020c9f2d947be766fe9227df18886e7b
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-06-30 17:25:44 +0200

    Add dlibgit version struct and assertions.

--- src/git/version_.d
+++ src/git/version_.d
@@ -6,13 +6,15 @@
  */
 module git.version_;
 
+import std.string;
+
 import git.c.version_;
 
 /**
-    Contains version information of $(LIBGIT2)
+    Version information of $(LIBGIT2)
     which this binding is based on.
 */
-struct GitVersion
+struct LibGitVersion
 {
     /// Text representation of version, e.g. "0.19.1"
     enum string text = LIBGIT2_VERSION;
@@ -26,3 +28,29 @@ struct GitVersion
     /// Revision version, e.g. 0.19.1 -> 1
     enum int revision = LIBGIT2_VER_REVISION;
 }
+
+/**
+    Version information of the $(D dlibgit) binding.
+    $(RED Note:) The $(D dlibgit) version specification
+    is separate from the $(LIBGIT2) version.
+*/
+struct DLibGitVersion
+{
+    /// Text representation of version, e.g. "0.19.1"
+    enum string text = format("%s.%s.%s", major, minor, revision);
+
+    /// Major version, e.g. 0.19.1 -> 0
+    enum int major = 0;
+
+    /// Minor version, e.g. 0.19.1 -> 19
+    enum int minor = 1;
+
+    /// Revision version, e.g. 0.19.1 -> 1
+    enum int revision = 0;
+}
+
+/// The libgit2 version this binding is based on
+static assert(LibGitVersion.text == "0.19.0");
+
+/// The version of the binding itself
+static assert(DLibGitVersion.text == "0.1.0");
