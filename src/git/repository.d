67bac97a806d1753468d26d2f0f363e19adf09db
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-07-02 20:26:41 +0200

    Add mergeMessage and fix typo.

--- src/git/repository.d
+++ src/git/repository.d
@@ -19,6 +19,7 @@ import std.string;
 import std.typecons;
 
 import git.c.common;
+import git.c.errors;
 import git.c.oid;
 import git.c.repository;
 import git.c.types;
@@ -292,6 +293,36 @@ struct GitRepo
         rmdirRecurse(_userRepo);
     }
 
+    /**
+        Retrieve git's prepared message for this repository.
+
+        Operations such as git revert/cherry-pick/merge with the -n option
+        stop just short of creating a commit with the changes and save
+        their prepared message in .git/MERGE_MSG so the next git-commit
+        execution can present it to the user for them to amend if they
+        wish.
+
+        Use this function to get the contents of this file. Don't forget to
+        remove the file after you create the commit.
+    */
+    @property string mergeMessage()
+    {
+        char[4096] buffer;
+        auto result = git_repository_message(buffer.ptr, buffer.length, _data._payload);
+
+        if (result == GIT_ENOTFOUND)
+            return null;
+
+        require(result >= 0);
+        return to!string(buffer.ptr);
+    }
+
+    ///
+    unittest
+    {
+        auto repo = GitRepo(_testRepo);
+        assert(repo.mergeMessage is null);
+    }
 private:
 
     /** Payload for the $(D git_repository) object which should be refcounted. */
@@ -472,216 +503,6 @@ unittest
 }
 
 extern (C):
-
-/**
- * Option flags for `git_repository_init_ext`.
- *
- * These flags configure extra behaviors to `git_repository_init_ext`.
- * In every case, the default behavior is the zero value (i.e. flag is
- * not set).  Just OR the flag values together for the `flags` parameter
- * when initializing a new repo.  Details of individual values are:
- *
- * * BARE   - Create a bare repository with no working directory.
- * * NO_REINIT - Return an EEXISTS error if the repo_path appears to
- *        already be an git repository.
- * * NO_DOTGIT_DIR - Normally a "/.git/" will be appended to the repo
- *        path for non-bare repos (if it is not already there), but
- *        passing this flag prevents that behavior.
- * * MKDIR  - Make the repo_path (and workdir_path) as needed.  Init is
- *        always willing to create the ".git" directory even without this
- *        flag.  This flag tells init to create the trailing component of
- *        the repo and workdir paths as needed.
- * * MKPATH - Recursively make all components of the repo and workdir
- *        paths as necessary.
- * * EXTERNAL_TEMPLATE - libgit2 normally uses internal templates to
- *        initialize a new repo.  This flags enables external templates,
- *        looking the "template_path" from the options if set, or the
- *        `init.templatedir` global config if not, or falling back on
- *        "/usr/share/git-core/templates" if it exists.
- */
-//~ enum git_repository_init_flag_t {
-        //~ GIT_REPOSITORY_INIT_BARE              = (1u << 0),
-        //~ GIT_REPOSITORY_INIT_NO_REINIT         = (1u << 1),
-        //~ GIT_REPOSITORY_INIT_NO_DOTGIT_DIR     = (1u << 2),
-        //~ GIT_REPOSITORY_INIT_MKDIR             = (1u << 3),
-        //~ GIT_REPOSITORY_INIT_MKPATH            = (1u << 4),
-        //~ GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE = (1u << 5),
-//~ } ;
-
-//~ mixin _ExportEnumMembers!git_repository_init_flag_t;
-
-/**
- * Mode options for `git_repository_init_ext`.
- *
- * Set the mode field of the `git_repository_init_options` structure
- * either to the custom mode that you would like, or to one of the
- * following modes:
- *
- * * SHARED_UMASK - Use permissions configured by umask - the default.
- * * SHARED_GROUP - Use "--shared=group" behavior, chmod'ing the new repo
- *        to be group writable and "g+sx" for sticky group assignment.
- * * SHARED_ALL - Use "--shared=all" behavior, adding world readability.
- * * Anything else - Set to custom value.
- */
-//~ enum git_repository_init_mode_t {
-        //~ GIT_REPOSITORY_INIT_SHARED_UMASK = octal!0,
-        //~ GIT_REPOSITORY_INIT_SHARED_GROUP = octal!2775,
-        //~ GIT_REPOSITORY_INIT_SHARED_ALL   = octal!2777,
-//~ } ;
-
-//~ mixin _ExportEnumMembers!git_repository_init_mode_t;
-
-/**
- * Extended options structure for `git_repository_init_ext`.
- *
- * This contains extra options for `git_repository_init_ext` that enable
- * additional initialization features.  The fields are:
- *
- * * flags - Combination of GIT_REPOSITORY_INIT flags above.
- * * mode  - Set to one of the standard GIT_REPOSITORY_INIT_SHARED_...
- *        constants above, or to a custom value that you would like.
- * * workdir_path - The path to the working dir or NULL for default (i.e.
- *        repo_path parent on non-bare repos).  IF THIS IS RELATIVE PATH,
- *        IT WILL BE EVALUATED RELATIVE TO THE REPO_PATH.  If this is not
- *        the "natural" working directory, a .git gitlink file will be
- *        created here linking to the repo_path.
- * * description - If set, this will be used to initialize the "description"
- *        file in the repository, instead of using the template content.
- * * template_path - When GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE is set,
- *        this contains the path to use for the template directory.  If
- *        this is NULL, the config or default directory options will be
- *        used instead.
- * * initial_head - The name of the head to point HEAD at.  If NULL, then
- *        this will be treated as "master" and the HEAD ref will be set
- *        to "refs/heads/master".  If this begins with "refs/" it will be
- *        used verbatim; otherwise "refs/heads/" will be prefixed.
- * * origin_url - If this is non-NULL, then after the rest of the
- *        repository initialization is completed, an "origin" remote
- *        will be added pointing to this URL.
- */
-struct git_repository_init_options {
-        uint version_;
-        uint32_t    flags;
-        uint32_t    mode;
-        const(char)* workdir_path;
-        const(char)* description;
-        const(char)* template_path;
-        const(char)* initial_head;
-        const(char)* origin_url;
-} ;
-
-enum GIT_REPOSITORY_INIT_OPTIONS_VERSION = 1;
-enum git_repository_init_options GIT_REPOSITORY_INIT_OPTIONS_INIT = { GIT_REPOSITORY_INIT_OPTIONS_VERSION };
-
-/**
- * Create a new Git repository in the given folder with extended controls.
- *
- * This will initialize a new git repository (creating the repo_path
- * if requested by flags) and working directory as needed.  It will
- * auto-detect the case sensitivity of the file system and if the
- * file system supports file mode bits correctly.
- *
- * @param out Pointer to the repo which will be created or reinitialized.
- * @param repo_path The path to the repository.
- * @param opts Pointer to git_repository_init_options struct.
- * @return 0 or an error code on failure.
- */
-int git_repository_init_ext(
-        git_repository **out_,
-        const(char)* repo_path,
-        git_repository_init_options *opts);
-
-/**
- * Get the configuration file for this repository.
- *
- * If a configuration file has not been set, the default
- * config set for the repository will be returned, including
- * global and system configurations (if they are available).
- *
- * The configuration file must be freed once it's no longer
- * being used by the user.
- *
- * @param out Pointer to store the loaded config file
- * @param repo A repository object
- * @return 0, or an error code
- */
-int git_repository_config(git_config **out_, git_repository *repo);
-
-/**
- * Get the Object Database for this repository.
- *
- * If a custom ODB has not been set, the default
- * database for the repository will be returned (the one
- * located in `.git/objects`).
- *
- * The ODB must be freed once it's no longer being used by
- * the user.
- *
- * @param out Pointer to store the loaded ODB
- * @param repo A repository object
- * @return 0, or an error code
- */
-int git_repository_odb(git_odb **out_, git_repository *repo);
-
-/**
- * Get the Reference Database Backend for this repository.
- *
- * If a custom refsdb has not been set, the default database for
- * the repository will be returned (the one that manipulates loose
- * and packed references in the `.git` directory).
- *
- * The refdb must be freed once it's no longer being used by
- * the user.
- *
- * @param out Pointer to store the loaded refdb
- * @param repo A repository object
- * @return 0, or an error code
- */
-int git_repository_refdb(git_refdb **out_, git_repository *repo);
-
-/**
- * Get the Index file for this repository.
- *
- * If a custom index has not been set, the default
- * index for the repository will be returned (the one
- * located in `.git/index`).
- *
- * The index must be freed once it's no longer being used by
- * the user.
- *
- * @param out Pointer to store the loaded index
- * @param repo A repository object
- * @return 0, or an error code
- */
-int git_repository_index(git_index **out_, git_repository *repo);
-
-/**
- * Retrieve git's prepared message
- *
- * Operations such as git revert/cherry-pick/merge with the -n option
- * stop just short of creating a commit with the changes and save
- * their prepared message in .git/MERGE_MSG so the next git-commit
- * execution can present it to the user for them to amend if they
- * wish.
- *
- * Use this function to get the contents of this file. Don't forget to
- * remove the file after you create the commit.
- *
- * If the repository message exists and there are no errors reading it, this
- * returns the bytes needed to store the message in memory (i.e. message
- * file size plus one terminating NUL byte).  That value is returned even if
- * `out` is NULL or `len` is shorter than the necessary size.
- *
- * The `out` buffer will *always* be NUL terminated, even if truncation
- * occurs.
- *
- * @param out Buffer to write data into or NULL to just read required size
- * @param len Length of `out` buffer in bytes
- * @param repo Repository to read prepared message from
- * @return GIT_ENOUTFOUND if no message exists, other value < 0 for other
- *         errors, or total bytes in message (may be > `len`) on success
- */
-int git_repository_message(char *out_, size_t len, git_repository *repo);
 
 /**
  * Remove git's prepared message.
@@ -879,6 +700,125 @@ int git_repository_is_shallow(git_repository *repo);
     TODO: Functions to wrap later:
 */
 
+
+/**
+ * Option flags for `git_repository_init_ext`.
+ *
+ * These flags configure extra behaviors to `git_repository_init_ext`.
+ * In every case, the default behavior is the zero value (i.e. flag is
+ * not set).  Just OR the flag values together for the `flags` parameter
+ * when initializing a new repo.  Details of individual values are:
+ *
+ * * BARE   - Create a bare repository with no working directory.
+ * * NO_REINIT - Return an EEXISTS error if the repo_path appears to
+ *        already be an git repository.
+ * * NO_DOTGIT_DIR - Normally a "/.git/" will be appended to the repo
+ *        path for non-bare repos (if it is not already there), but
+ *        passing this flag prevents that behavior.
+ * * MKDIR  - Make the repo_path (and workdir_path) as needed.  Init is
+ *        always willing to create the ".git" directory even without this
+ *        flag.  This flag tells init to create the trailing component of
+ *        the repo and workdir paths as needed.
+ * * MKPATH - Recursively make all components of the repo and workdir
+ *        paths as necessary.
+ * * EXTERNAL_TEMPLATE - libgit2 normally uses internal templates to
+ *        initialize a new repo.  This flags enables external templates,
+ *        looking the "template_path" from the options if set, or the
+ *        `init.templatedir` global config if not, or falling back on
+ *        "/usr/share/git-core/templates" if it exists.
+ */
+//~ enum git_repository_init_flag_t {
+        //~ GIT_REPOSITORY_INIT_BARE              = (1u << 0),
+        //~ GIT_REPOSITORY_INIT_NO_REINIT         = (1u << 1),
+        //~ GIT_REPOSITORY_INIT_NO_DOTGIT_DIR     = (1u << 2),
+        //~ GIT_REPOSITORY_INIT_MKDIR             = (1u << 3),
+        //~ GIT_REPOSITORY_INIT_MKPATH            = (1u << 4),
+        //~ GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE = (1u << 5),
+//~ } ;
+
+//~ mixin _ExportEnumMembers!git_repository_init_flag_t;
+
+/**
+ * Mode options for `git_repository_init_ext`.
+ *
+ * Set the mode field of the `git_repository_init_options` structure
+ * either to the custom mode that you would like, or to one of the
+ * following modes:
+ *
+ * * SHARED_UMASK - Use permissions configured by umask - the default.
+ * * SHARED_GROUP - Use "--shared=group" behavior, chmod'ing the new repo
+ *        to be group writable and "g+sx" for sticky group assignment.
+ * * SHARED_ALL - Use "--shared=all" behavior, adding world readability.
+ * * Anything else - Set to custom value.
+ */
+//~ enum git_repository_init_mode_t {
+        //~ GIT_REPOSITORY_INIT_SHARED_UMASK = octal!0,
+        //~ GIT_REPOSITORY_INIT_SHARED_GROUP = octal!2775,
+        //~ GIT_REPOSITORY_INIT_SHARED_ALL   = octal!2777,
+//~ } ;
+
+//~ mixin _ExportEnumMembers!git_repository_init_mode_t;
+
+/**
+ * Extended options structure for `git_repository_init_ext`.
+ *
+ * This contains extra options for `git_repository_init_ext` that enable
+ * additional initialization features.  The fields are:
+ *
+ * * flags - Combination of GIT_REPOSITORY_INIT flags above.
+ * * mode  - Set to one of the standard GIT_REPOSITORY_INIT_SHARED_...
+ *        constants above, or to a custom value that you would like.
+ * * workdir_path - The path to the working dir or NULL for default (i.e.
+ *        repo_path parent on non-bare repos).  IF THIS IS RELATIVE PATH,
+ *        IT WILL BE EVALUATED RELATIVE TO THE REPO_PATH.  If this is not
+ *        the "natural" working directory, a .git gitlink file will be
+ *        created here linking to the repo_path.
+ * * description - If set, this will be used to initialize the "description"
+ *        file in the repository, instead of using the template content.
+ * * template_path - When GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE is set,
+ *        this contains the path to use for the template directory.  If
+ *        this is NULL, the config or default directory options will be
+ *        used instead.
+ * * initial_head - The name of the head to point HEAD at.  If NULL, then
+ *        this will be treated as "master" and the HEAD ref will be set
+ *        to "refs/heads/master".  If this begins with "refs/" it will be
+ *        used verbatim; otherwise "refs/heads/" will be prefixed.
+ * * origin_url - If this is non-NULL, then after the rest of the
+ *        repository initialization is completed, an "origin" remote
+ *        will be added pointing to this URL.
+ */
+struct git_repository_init_options {
+        uint version_;
+        uint32_t    flags;
+        uint32_t    mode;
+        const(char)* workdir_path;
+        const(char)* description;
+        const(char)* template_path;
+        const(char)* initial_head;
+        const(char)* origin_url;
+} ;
+
+enum GIT_REPOSITORY_INIT_OPTIONS_VERSION = 1;
+enum git_repository_init_options GIT_REPOSITORY_INIT_OPTIONS_INIT = { GIT_REPOSITORY_INIT_OPTIONS_VERSION };
+
+/**
+ * Create a new Git repository in the given folder with extended controls.
+ *
+ * This will initialize a new git repository (creating the repo_path
+ * if requested by flags) and working directory as needed.  It will
+ * auto-detect the case sensitivity of the file system and if the
+ * file system supports file mode bits correctly.
+ *
+ * @param out Pointer to the repo which will be created or reinitialized.
+ * @param repo_path The path to the repository.
+ * @param opts Pointer to git_repository_init_options struct.
+ * @return 0 or an error code on failure.
+ */
+int git_repository_init_ext(
+        git_repository **out_,
+        const(char)* repo_path,
+        git_repository_init_options *opts);
+
 /**
  * Create a "fake" repository to wrap an object database
  *
@@ -922,3 +862,72 @@ int git_repository_open_bare(git_repository **out_, const(char)* bare_path);
  */
 // todo: when git_reference is ported
 int git_repository_head(git_reference **out_, git_repository *repo);
+
+
+/**
+ * Get the configuration file for this repository.
+ *
+ * If a configuration file has not been set, the default
+ * config set for the repository will be returned, including
+ * global and system configurations (if they are available).
+ *
+ * The configuration file must be freed once it's no longer
+ * being used by the user.
+ *
+ * @param out Pointer to store the loaded config file
+ * @param repo A repository object
+ * @return 0, or an error code
+ */
+// todo: when git_config is ported
+int git_repository_config(git_config **out_, git_repository *repo);
+
+/**
+ * Get the Object Database for this repository.
+ *
+ * If a custom ODB has not been set, the default
+ * database for the repository will be returned (the one
+ * located in `.git/objects`).
+ *
+ * The ODB must be freed once it's no longer being used by
+ * the user.
+ *
+ * @param out Pointer to store the loaded ODB
+ * @param repo A repository object
+ * @return 0, or an error code
+ */
+// todo: when git_odb is ported
+int git_repository_odb(git_odb **out_, git_repository *repo);
+
+/**
+ * Get the Reference Database Backend for this repository.
+ *
+ * If a custom refsdb has not been set, the default database for
+ * the repository will be returned (the one that manipulates loose
+ * and packed references in the `.git` directory).
+ *
+ * The refdb must be freed once it's no longer being used by
+ * the user.
+ *
+ * @param out Pointer to store the loaded refdb
+ * @param repo A repository object
+ * @return 0, or an error code
+ */
+// todo: when git_refdb is ported
+int git_repository_refdb(git_refdb **out_, git_repository *repo);
+
+/**
+ * Get the Index file for this repository.
+ *
+ * If a custom index has not been set, the default
+ * index for the repository will be returned (the one
+ * located in `.git/index`).
+ *
+ * The index must be freed once it's no longer being used by
+ * the user.
+ *
+ * @param out Pointer to store the loaded index
+ * @param repo A repository object
+ * @return 0, or an error code
+ */
+// todo: when git_index is ported
+int git_repository_index(git_index **out_, git_repository *repo);
