20bce83255654ded84070ef76b00bbd3a40fa618
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-07-03 23:17:23 +0200

    Fix build script.

--- build/build.bat
+++ build/build.bat
@@ -3,8 +3,8 @@ setlocal EnableDelayedExpansion
 
 set thisPath=%~dp0
 set binPath=%thisPath%\..\bin
-
-cd %thisPath%\..\src
+set srcPath=%thisPath%\..\src
+cd %srcPath%
 
 set "files="
 for /r %%i in (git\*.d) do set files=%%i !files!
@@ -15,12 +15,15 @@ rem set compiler=dmd.exe
 set compiler=dmd_msc.exe
 rem set compiler=ldmd2.exe
 
-set "flags=%binPath%\libgit2_implib.lib"
+set "implibFile=%binPath%\libgit2_implib.lib"
+
+set "flags=%implibFile%"
+
+if not exist %implibFile% cd %binPath% && call make_implib.bat > NUL && cd %srcPath%
 
 rem Note: -g option disabled due to CodeView bugs which crash linkers
 rem (both Optlink and Unilink will ICE)
 set dtest=rdmd -g --main -debug -unittest --force -of%binPath%\dlibgit_test.exe
 
-%dtest% --compiler=%compiler% %flags% -Isrc git\package.d
-rem &&
-rem echo %compiler% -of%binPath%\dlibgit.lib -lib %flags% -g %files%
+%dtest% --compiler=%compiler% %flags% -Isrc git\package.d && echo Success: dlibgit tested ok.
+rem %compiler% -of%binPath%\dlibgit.lib -lib %flags% %files% && echo Success: dlibgit built ok.
