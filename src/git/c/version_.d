30b87e26211a6d9126e09c6d1d7a909a2e70a024
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-02-08 17:23:11 +0100

    Remove the C interface and use the new libgit2 package instead.

--- src/git/c/version_.d
+++ src/git/c/version_.d
@@ -1,15 +1,3 @@
+// This file is just a placeholder, please use the libgit2 package directly instead
 module git.c.version_;
-
-/*
- * Copyright (C) the libgit2 contributors. All rights reserved.
- *
- * This file is part of libgit2, distributed under the GNU GPL v2 with
- * a Linking Exception. For full terms see the included COPYING file.
- */
-
-extern (C):
-
-enum string LIBGIT2_VERSION = "0.19.0";
-enum int LIBGIT2_VER_MAJOR = 0;
-enum int LIBGIT2_VER_MINOR = 19;
-enum int LIBGIT2_VER_REVISION = 0;
+public import git2.version_;
